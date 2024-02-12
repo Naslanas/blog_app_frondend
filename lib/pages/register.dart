0258d7060166abda968fe5e47c305edbf84bc784
Naslanas <naslava00@gmail.com>
2024-02-12 13:42:22 +0530

    Login page updated

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,3 +1,4 @@
+import 'package:blog/Service/blogService.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -8,6 +9,34 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  String Name="",Age="",Mobile="",Address="",Pincode="",Email="",Password="",getName="",getAge="",getMobile="",getAddress="",getPincode="",
+  getEmail="",getPass="";
+  TextEditingController name=new TextEditingController();
+  TextEditingController age=new TextEditingController();
+  TextEditingController mobile=new TextEditingController();
+  TextEditingController address=new TextEditingController();
+  TextEditingController pincode=new TextEditingController();
+  TextEditingController email=new TextEditingController();
+  TextEditingController password=new TextEditingController();
+  void SendValuesToApi()async{
+    getName=name.text.toString();
+    getAge=age.text.toString();
+    getMobile=age.text.toString();
+    getAddress=address.text.toString();
+    getPincode=pincode.text.toString();
+    getEmail=email.text.toString();
+    getPass=password.text.toString();
+
+    final response=await BlogApiService().sendData(getName, getAge, getMobile, getAddress, getPincode, getEmail, getPass);
+    if(response["status"]=="success"){
+      print("Successfully Added...");
+    }
+    else{
+      print("Error.....");
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,66 +45,68 @@ class _RegisterState extends State<Register> {
           backgroundColor: Colors.lightBlue,
           title: Text("Blog App"),
         ),
-        body: Container(
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "Name",
-                    border: OutlineInputBorder()
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                TextField(controller: name,
+                  decoration: InputDecoration(
+                      labelText: "Name",
+                      border: OutlineInputBorder()
+                  ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "age",
-                    border: OutlineInputBorder()
+                SizedBox(height: 10,),
+                TextField(controller: age,
+                  decoration: InputDecoration(
+                      labelText: "age",
+                      border: OutlineInputBorder()
+                  ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "Mobile",
-                    border: OutlineInputBorder()
+                SizedBox(height: 10,),
+                TextField(controller: mobile,
+                  decoration: InputDecoration(
+                      labelText: "Mobile",
+                      border: OutlineInputBorder()
+                  ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "Address",
-                    border: OutlineInputBorder()
+                SizedBox(height: 10,),
+                TextField(controller: address,
+                  decoration: InputDecoration(
+                      labelText: "Address",
+                      border: OutlineInputBorder()
+                  ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "Pincode",
-                    border: OutlineInputBorder()
+                SizedBox(height: 10,),
+                TextField(controller: pincode,
+                  decoration: InputDecoration(
+                      labelText: "Pincode",
+                      border: OutlineInputBorder()
+                  ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "email",
-                    border: OutlineInputBorder()
+                SizedBox(height: 10,),
+                TextField(controller: email,
+                  decoration: InputDecoration(
+                      labelText: "email",
+                      border: OutlineInputBorder()
+                  ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "password",
-                    border: OutlineInputBorder()
+                SizedBox(height: 10,),
+                TextField(controller: password,
+                  decoration: InputDecoration(
+                      labelText: "password",
+                      border: OutlineInputBorder()
+                  ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){}, child: Text("Register")),
-              SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){
-                Navigator.pop(context);
-              }, child: Text("Back"))
-
-
-            ],
+                SizedBox(height: 10,),
+                ElevatedButton(onPressed: SendValuesToApi, child: Text("Register")),
+                SizedBox(height: 10,),
+                ElevatedButton(onPressed: (){
+                  Navigator.pop(context);
+                }, child: Text("Back"))
+          
+          
+              ],
+            ),
           ),
         ),
       ),
